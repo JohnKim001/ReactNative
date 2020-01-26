c4d70562548b65c0ca9713b8d37d95c6af47d8ea
John Kim <gkdudchl01@gmail.com>
2020-01-25 17:28:13 -0800

    ReactNative

---
+++ shared/baseUrl.js
@@ -0,0 +1,4 @@
+//export const baseUrl = 'http://10.0.0.10:3001/';
+export const baseUrl = 'http://10.167.68.213:3001/';
+//export const baseUrl = 'http://10.228.140.229:3001/';
+//export const baseUrl = 'http://10.236.7.164:3001/';
